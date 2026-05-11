8d691e829eadfa7fdbcef40f565f95bee568e4f0
Muhammad Rezki Khaidir <rezkihiddir@gmail.com>
2026-05-11 15:30:40 +0700

    Pertemuan 12

---
+++ Pertemuan 11-11 Mei 2026/script.js
@@ -0,0 +1,9 @@
+// Fungsi sederhana untuk menyapa pengunjung
+function sapaUser() {
+  alert("Halo! Jarvis di sini. Sudah siap kulineran di Bogor hari ini?");
+}
+
+// Logika ini akan jalan saat halaman selesai dimuat
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Website Makan Kuy! Bogor siap dijalankan.");
+});
